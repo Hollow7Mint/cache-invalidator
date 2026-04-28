4e2b6b6a4b0f75557680b98f88bffdae1dda5de1
James Holloway <james.holloway94@gmail.com>
2026-04-28 07:06:52 +0000

    backfill s3 versioning sync marker in metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-04-28 05:27:08 UTC
+# Last sync: 2026-04-28 07:06:52 UTC
