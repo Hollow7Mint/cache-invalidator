8933f813cdb3a6f38912e8c1f351fae4c88c246d
James Holloway <james.holloway94@gmail.com>
2026-04-28 20:59:54 +0000

    save bastion policy sync marker after review

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-04-28 19:14:28 UTC
+# Last sync: 2026-04-28 20:59:54 UTC
