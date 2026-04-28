cf2d3d8c461aca85216f22e7c3ae9b0150190620
James Holloway <james.holloway94@gmail.com>
2026-04-28 09:07:37 +0000

    recheck ecs service sync after deployment window

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-04-28 08:31:17 UTC
+# Last sync: 2026-04-28 09:07:37 UTC
