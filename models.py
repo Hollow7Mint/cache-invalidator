f3dac7e8dc75383a64d9bf7f13f022d2beb07f75
James Holloway <james.holloway94@gmail.com>
2026-04-29 02:11:26 +0000

    flush ec2 fleet inventory marker after scan

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-04-29 01:19:02 UTC
+# Last sync: 2026-04-29 02:11:26 UTC
