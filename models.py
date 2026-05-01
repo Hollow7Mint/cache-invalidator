63d5a9c0fa14a19b090fc25a4ca0f9f293c555cc
James Holloway <james.holloway94@gmail.com>
2026-05-01 06:42:30 +0000

    capture host key sync completion time in record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-01 05:17:53 UTC
+# Last sync: 2026-05-01 06:42:30 UTC
