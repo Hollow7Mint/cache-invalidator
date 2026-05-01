9588fac6f758ee449c7da14ef2a27e281b3c8174
James Holloway <james.holloway94@gmail.com>
2026-05-01 04:08:50 +0000

    resync parameter store path time for nightly audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-01 03:46:35 UTC
+# Last sync: 2026-05-01 04:08:50 UTC
