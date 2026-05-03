15e80a8cdee96f1e34142da9d44ce3f428a519c1
James Holloway <james.holloway94@gmail.com>
2026-05-03 07:53:10 +0000

    reconcile cloudwatch dashboard sync after update

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-03 06:41:16 UTC
+# Last sync: 2026-05-03 07:53:10 UTC
