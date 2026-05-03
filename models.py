132de8636db6f279da6d268cdda25d8bb15a3432
James Holloway <james.holloway94@gmail.com>
2026-05-03 00:10:14 +0000

    bridge ssh ProxyJump config sync through banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-02 23:58:08 UTC
+# Last sync: 2026-05-03 00:10:14 UTC
