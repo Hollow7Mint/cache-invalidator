47b393dd94838e9a2050a4a780b9670b886ca8c3
James Holloway <james.holloway94@gmail.com>
2026-05-07 02:01:20 +0000

    flush mongodb replica lag marker for nightly check

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-07 00:25:19 UTC
+# Last sync: 2026-05-07 02:01:20 UTC
