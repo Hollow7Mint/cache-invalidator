eaa17fea59b31940acdacfa3749d87593505d43b
James Holloway <james.holloway94@gmail.com>
2026-05-07 03:35:36 +0000

    finish mongodb sharding sync heartbeat after balance

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-07 02:37:35 UTC
+# Last sync: 2026-05-07 03:35:36 UTC
