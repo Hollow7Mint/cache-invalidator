b9d274b359d1ab3fec90032c23cbad7ccc669042
James Holloway <james.holloway94@gmail.com>
2026-05-08 09:42:29 +0000

    document ssh host key sync time in record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-08 08:41:03 UTC
+# Last sync: 2026-05-08 09:42:29 UTC
