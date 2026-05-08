52b89b686440ae939f9c7e1ebddd9013a733ad1a
James Holloway <james.holloway94@gmail.com>
2026-05-08 22:26:15 +0000

    harvest postgres vacuum sync for nightly audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-08 21:38:37 UTC
+# Last sync: 2026-05-08 22:26:15 UTC
