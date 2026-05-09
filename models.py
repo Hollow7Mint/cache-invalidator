dbf692121414624a565b307ec28bb47cb7456bed
James Holloway <james.holloway94@gmail.com>
2026-05-09 22:48:53 +0000

    chart autoscale sync time in deployment metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-09 21:54:57 UTC
+# Last sync: 2026-05-09 22:48:52 UTC
