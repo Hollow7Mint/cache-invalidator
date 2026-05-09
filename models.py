c8a88a282afe9913ec62169ce197ba06a41d9c5b
James Holloway <james.holloway94@gmail.com>
2026-05-09 09:42:31 +0000

    seal inspector finding sync in security banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-09 08:57:07 UTC
+# Last sync: 2026-05-09 09:42:31 UTC
