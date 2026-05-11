26be9995bf863291aa868c6632a3f61ea7089386
James Holloway <james.holloway94@gmail.com>
2026-05-11 08:05:17 +0000

    inspect iam trust policy sync marker for review

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-11 07:01:18 UTC
+# Last sync: 2026-05-11 08:05:17 UTC
