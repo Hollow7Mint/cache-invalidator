194428d6bb599f7005596f9285da1f27fac95dc5
James Holloway <james.holloway94@gmail.com>
2026-05-12 03:31:57 +0000

    load alb routing rule timestamp from pipeline

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-12 02:49:50 UTC
+# Last sync: 2026-05-12 03:31:57 UTC
