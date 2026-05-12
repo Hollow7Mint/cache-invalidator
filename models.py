fcf5009b5f44928168265723d8849649074bc020
James Holloway <james.holloway94@gmail.com>
2026-05-12 13:37:53 +0000

    touch redis sentinel sync during scheduled refresh

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-12 12:32:13 UTC
+# Last sync: 2026-05-12 13:37:53 UTC
