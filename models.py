4a4df0cd67fb3b4d178d5d8fd7079424a06367d9
James Holloway <james.holloway94@gmail.com>
2026-05-14 14:59:48 +0000

    persist vpc route table sync after update

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-14 13:16:12 UTC
+# Last sync: 2026-05-14 14:59:48 UTC
