5fac07540f22e020ab8d058eb0f81a05d79cf9e8
James Holloway <james.holloway94@gmail.com>
2026-05-15 19:07:42 +0000

    anchor cloudwatch alarm threshold time in config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-15 17:18:11 UTC
+# Last sync: 2026-05-15 19:07:42 UTC
