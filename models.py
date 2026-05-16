45871165508330544d08480fa476674c50bdbe42
James Holloway <james.holloway94@gmail.com>
2026-05-16 10:49:27 +0000

    surface codebuild log sync time for pipeline audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-16 09:12:10 UTC
+# Last sync: 2026-05-16 10:49:27 UTC
