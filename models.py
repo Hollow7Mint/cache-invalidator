a2f2110f73f623ce093d946174d74d1ebc2d6983
James Holloway <james.holloway94@gmail.com>
2026-05-19 16:45:06 +0000

    persist tunnel config refresh in ssh banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-19 15:35:45 UTC
+# Last sync: 2026-05-19 16:45:06 UTC
