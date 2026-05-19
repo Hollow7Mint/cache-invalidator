c08edbd991363b2bb226ce2a051d2a78db812db8
James Holloway <james.holloway94@gmail.com>
2026-05-19 18:06:59 +0000

    refresh aws config sync marker after nightly reconcile

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-19 16:45:06 UTC
+# Last sync: 2026-05-19 18:06:59 UTC
