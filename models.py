4419bba428e95e944c45cb47b73d2f4959c351bb
James Holloway <james.holloway94@gmail.com>
2026-05-19 15:03:17 +0000

    resolve mysql backup sync into metadata file

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-19 13:52:13 UTC
+# Last sync: 2026-05-19 15:03:17 UTC
