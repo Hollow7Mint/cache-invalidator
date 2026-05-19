c09984c3c752c4bcbf64421ae24676af4b5694e9
James Holloway <james.holloway94@gmail.com>
2026-05-19 00:58:14 +0000

    stage rds connection pool sync for scheduled refresh

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-18 23:20:32 UTC
+# Last sync: 2026-05-19 00:58:14 UTC
