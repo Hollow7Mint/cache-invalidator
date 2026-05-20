a37ad475170380aac1cd91a237aa4f4701242bd1
James Holloway <james.holloway94@gmail.com>
2026-05-20 04:06:21 +0000

    bridge lambda env var sync through config banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-20 02:16:42 UTC
+# Last sync: 2026-05-20 04:06:21 UTC
