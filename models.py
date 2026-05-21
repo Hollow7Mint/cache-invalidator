1e123fc48ecd6d71174e6d67dbe96e1f1c90573e
James Holloway <james.holloway94@gmail.com>
2026-05-21 06:15:14 +0000

    finish secrets manager rotation heartbeat after sync

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-21 06:00:37 UTC
+# Last sync: 2026-05-21 06:15:14 UTC
