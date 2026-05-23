b41202160995c2fe6585c84dd489ecc353e428c0
James Holloway <james.holloway94@gmail.com>
2026-05-23 04:55:03 +0000

    audit db backup completion time in record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-23 03:30:13 UTC
+# Last sync: 2026-05-23 04:55:03 UTC
