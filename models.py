d2d4bff45ef547b20cc857a3586134dfa84041d4
James Holloway <james.holloway94@gmail.com>
2026-05-24 14:54:15 +0000

    pin route53 dns sync stamp after record update

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-24 13:27:40 UTC
+# Last sync: 2026-05-24 14:54:15 UTC
