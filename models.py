be8efeae0ab9189d01ae2b808e87c9198ffbe2cd
James Holloway <james.holloway94@gmail.com>
2026-05-26 14:02:23 +0000

    lift asg capacity sync time into deploy banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-26 12:59:56 UTC
+# Last sync: 2026-05-26 14:02:23 UTC
