564f756313f5e2c778e62ed18aa1b07fec41e1a5
James Holloway <james.holloway94@gmail.com>
2026-05-26 18:03:10 +0000

    relay db connection string time through config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-26 16:20:38 UTC
+# Last sync: 2026-05-26 18:03:10 UTC
