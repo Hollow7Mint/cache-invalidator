4d155bd026cf1e60248f3c609e7f8c9f818c4868
James Holloway <james.holloway94@gmail.com>
2026-05-27 16:47:01 +0000

    run guardduty detector heartbeat for scheduled audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-27 15:21:36 UTC
+# Last sync: 2026-05-27 16:47:01 UTC
