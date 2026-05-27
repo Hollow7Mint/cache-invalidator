b879816e72fb69c17dd0e62cc62a41925e7d6648
James Holloway <james.holloway94@gmail.com>
2026-05-27 09:25:43 +0000

    stage bastion jump host sync for scheduled refresh

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-27 08:15:41 UTC
+# Last sync: 2026-05-27 09:25:43 UTC
