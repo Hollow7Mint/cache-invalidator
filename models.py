01d4b1c01fe8568c87b4c5c259fb36976b41d686
James Holloway <james.holloway94@gmail.com>
2026-05-27 04:56:24 +0000

    queue cloudfront distribution stamp after invalidation

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-27 03:20:57 UTC
+# Last sync: 2026-05-27 04:56:24 UTC
