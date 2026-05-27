86a15f2097e2757d1fb7a55402181a11a1309e5a
James Holloway <james.holloway94@gmail.com>
2026-05-27 20:53:53 +0000

    thread cloudtrail digest sync through config banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-27 19:50:29 UTC
+# Last sync: 2026-05-27 20:53:53 UTC
