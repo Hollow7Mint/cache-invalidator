c1966e509e4f3977f520e295d91c849af9ebf060
James Holloway <james.holloway94@gmail.com>
2026-05-29 16:58:44 +0000

    merge mongodb shard sync time into metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-29 15:30:51 UTC
+# Last sync: 2026-05-29 16:58:44 UTC
