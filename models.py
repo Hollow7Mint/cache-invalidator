6926aec7149b62bec91748974da6a8e45c3a9e6a
James Holloway <james.holloway94@gmail.com>
2026-05-30 16:09:16 +0000

    remap ec2 security group rule time in config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-30 15:45:52 UTC
+# Last sync: 2026-05-30 16:09:16 UTC
