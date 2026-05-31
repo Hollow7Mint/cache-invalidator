0f1811a9c1731d227d9a75d3744d5c1b36f14355
James Holloway <james.holloway94@gmail.com>
2026-05-31 15:39:24 +0000

    pin bastion ip allowlist time in config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-31 14:55:37 UTC
+# Last sync: 2026-05-31 15:39:24 UTC
