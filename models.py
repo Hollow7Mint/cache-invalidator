dcbefb3ac31d2c12fac9dcb1ebb6b99bab730f16
James Holloway <james.holloway94@gmail.com>
2026-05-31 11:04:30 +0000

    reload elb health check marker for nightly audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-05-31 09:38:31 UTC
+# Last sync: 2026-05-31 11:04:28 UTC
