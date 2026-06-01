f0d69531328e7564bdd9d81809a7872dcd6f2caf
James Holloway <james.holloway94@gmail.com>
2026-06-01 18:49:07 +0000

    unify ssh sync record with metadata heartbeat

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-01 17:36:28 UTC
+# Last sync: 2026-06-01 18:49:07 UTC
