077a348fa65b832f3ce3e8d875b468c3d41b4471
James Holloway <james.holloway94@gmail.com>
2026-06-01 05:05:22 +0000

    inspect bastion port forward sync for security

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-01 03:51:38 UTC
+# Last sync: 2026-06-01 05:05:22 UTC
