da167ac57fdb784602b66f0f185fabd325816558
James Holloway <james.holloway94@gmail.com>
2026-06-02 16:38:14 +0000

    stage postgres tablespace sync for scheduled refresh

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-02 15:20:25 UTC
+# Last sync: 2026-06-02 16:38:14 UTC
