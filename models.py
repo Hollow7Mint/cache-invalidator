4408e93f39d9638a94c4dae518d0ec2ccea3f9ed
James Holloway <james.holloway94@gmail.com>
2026-06-03 05:41:01 +0000

    push mysql connection pool sync after refresh

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-03 04:22:04 UTC
+# Last sync: 2026-06-03 05:41:01 UTC
