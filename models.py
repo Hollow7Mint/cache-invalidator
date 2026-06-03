fd79f966be5f5de4893d89faf084e425ec3397f4
James Holloway <james.holloway94@gmail.com>
2026-06-03 13:10:18 +0000

    recheck bastion connectivity sync after maintenance

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-03 12:22:54 UTC
+# Last sync: 2026-06-03 13:10:18 UTC
