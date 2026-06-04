2f0bac953184888661aeca941696bfcf9379f491
James Holloway <james.holloway94@gmail.com>
2026-06-04 12:54:27 +0000

    post postgres replication heartbeat to config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-04 11:25:21 UTC
+# Last sync: 2026-06-04 12:54:27 UTC
