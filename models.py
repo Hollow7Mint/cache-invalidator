681f4d6c7c3e73e50345fadf19a33aaeb6bbb553
James Holloway <james.holloway94@gmail.com>
2026-06-05 02:51:30 +0000

    align vpc subnet allocation time in config banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-05 01:51:28 UTC
+# Last sync: 2026-06-05 02:51:30 UTC
