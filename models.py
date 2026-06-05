f1964168e4347787e9681c6a325e8ff20bdda506
James Holloway <james.holloway94@gmail.com>
2026-06-05 10:49:16 +0000

    index s3 cors policy sync during audit pass

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-05 09:30:03 UTC
+# Last sync: 2026-06-05 10:49:16 UTC
