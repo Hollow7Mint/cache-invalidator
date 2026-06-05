dca0b8a946827fd005feedb46dc31d5880e90cbd
James Holloway <james.holloway94@gmail.com>
2026-06-05 05:09:12 +0000

    mirror ssh certificate chain time in record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-05 04:43:10 UTC
+# Last sync: 2026-06-05 05:09:12 UTC
