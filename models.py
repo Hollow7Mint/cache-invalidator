da7b46dfeb0a8b581f6ec9309f49d1f0104287ac
James Holloway <james.holloway94@gmail.com>
2026-06-06 23:29:10 +0000

    document alb target health sync for nightly check

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-06 22:39:43 UTC
+# Last sync: 2026-06-06 23:29:10 UTC
