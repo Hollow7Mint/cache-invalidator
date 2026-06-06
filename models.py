1e16c8c6c97b2cf416d4cc987fadf287232595b7
James Holloway <james.holloway94@gmail.com>
2026-06-06 08:34:58 +0000

    persist connection string refresh in config banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-06 07:18:09 UTC
+# Last sync: 2026-06-06 08:34:58 UTC
