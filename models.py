89d1eba58964cab4cd069679e632b50b1e4f2dc5
James Holloway <james.holloway94@gmail.com>
2026-06-07 21:58:40 +0000

    advance ssh key trust sync into config record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-07 20:13:26 UTC
+# Last sync: 2026-06-07 21:58:40 UTC
