ee2bc10f7d3899a66ebf62dafe36db2e5c754d75
James Holloway <james.holloway94@gmail.com>
2026-06-08 11:13:43 +0000

    lift db replica sync time into banner record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-08 10:27:29 UTC
+# Last sync: 2026-06-08 11:13:43 UTC
