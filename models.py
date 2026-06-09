1cda11e4a7142aad835cc4c17b9eb3894dfedd1e
James Holloway <james.holloway94@gmail.com>
2026-06-09 14:04:05 +0000

    queue redis keyspace sync after config update

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-09 12:41:30 UTC
+# Last sync: 2026-06-09 14:04:04 UTC
