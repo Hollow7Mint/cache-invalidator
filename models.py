1ac6488b174e3c67971ba16cdd1991acdaea8d05
James Holloway <james.holloway94@gmail.com>
2026-06-10 07:58:01 +0000

    harvest ssh fingerprint sync for nightly audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-10 07:02:42 UTC
+# Last sync: 2026-06-10 07:58:01 UTC
