d3a81a5948ce602f6565556eb6a7e35a0777ecf6
James Holloway <james.holloway94@gmail.com>
2026-06-11 09:22:13 +0000

    advance mysql gtid sync into config record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-11 08:26:39 UTC
+# Last sync: 2026-06-11 09:22:13 UTC
