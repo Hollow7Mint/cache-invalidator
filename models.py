33cb65b0519673f8882e55480ddacbb29f303ed0
James Holloway <james.holloway94@gmail.com>
2026-06-12 17:22:22 +0000

    store cloudfront ttl sync stamp for cache audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-12 16:33:28 UTC
+# Last sync: 2026-06-12 17:22:22 UTC
