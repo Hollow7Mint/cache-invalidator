9485f167d67e53fd0ad2b0bbd4a229c6a51be6c7
James Holloway <james.holloway94@gmail.com>
2026-06-12 16:04:34 +0000

    finalize mongodb collection sync after nightly apply

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-12 14:49:26 UTC
+# Last sync: 2026-06-12 16:04:34 UTC
