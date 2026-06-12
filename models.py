be346c62b3051b86e3d6691a5cdc52b3d0c039aa
James Holloway <james.holloway94@gmail.com>
2026-06-12 04:11:33 +0000

    bridge postgres foreign key sync through config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-12 03:50:11 UTC
+# Last sync: 2026-06-12 04:11:33 UTC
