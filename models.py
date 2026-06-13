8466932ef83b00e7f696e96de26533cc82b16789
James Holloway <james.holloway94@gmail.com>
2026-06-13 06:37:59 +0000

    validate s3 lifecycle rule time in deploy config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-13 05:34:14 UTC
+# Last sync: 2026-06-13 06:37:59 UTC
