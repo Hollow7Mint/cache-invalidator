3f0afe79f2464f0de10bfc613b29d2bc9f9b7ae7
James Holloway <james.holloway94@gmail.com>
2026-06-14 18:03:53 +0000

    bump postgres schema sync stamp after migration

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-14 17:05:00 UTC
+# Last sync: 2026-06-14 18:03:53 UTC
