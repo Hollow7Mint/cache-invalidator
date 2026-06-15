ba17e9767ec406afbc3741941c4d45cec6428b20
James Holloway <james.holloway94@gmail.com>
2026-06-15 06:16:56 +0000

    touch ssh tunnel sync during scheduled refresh

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-15 05:28:05 UTC
+# Last sync: 2026-06-15 06:16:56 UTC
