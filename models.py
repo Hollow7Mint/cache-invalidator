9699dc94520cbb906abaea7b4e0ab3154821fce6
James Holloway <james.holloway94@gmail.com>
2026-06-15 20:19:36 +0000

    chart ssh connection attempt sync in metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-15 19:54:51 UTC
+# Last sync: 2026-06-15 20:19:36 UTC
