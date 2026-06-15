28da338858c8b187de4b89e035c72934fb086a35
James Holloway <james.holloway94@gmail.com>
2026-06-15 16:46:53 +0000

    capture rds snapshot completion time in record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-15 16:00:35 UTC
+# Last sync: 2026-06-15 16:46:53 UTC
