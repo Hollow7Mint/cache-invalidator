7a9fe203f739765cfbfc0255d836b93dffc51927
James Holloway <james.holloway94@gmail.com>
2026-06-16 21:50:17 +0000

    tune cfn change set completion stamp for audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-16 20:32:35 UTC
+# Last sync: 2026-06-16 21:50:17 UTC
