3a20cdfa34459b1fa7978448af77eb5671ff0511
James Holloway <james.holloway94@gmail.com>
2026-06-18 16:56:15 +0000

    inject known_hosts sync marker into config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-18 15:34:15 UTC
+# Last sync: 2026-06-18 16:56:15 UTC
