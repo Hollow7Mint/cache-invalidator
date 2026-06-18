c99fc63dc2184e6e58ff4e2bb74f93b4e8b7aef7
James Holloway <james.holloway94@gmail.com>
2026-06-18 14:45:13 +0000

    run mongodb heartbeat for scheduled audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-18 14:02:26 UTC
+# Last sync: 2026-06-18 14:45:13 UTC
