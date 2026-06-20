29585a99a95da49677ecaf6d44f0a27223edb0b5
James Holloway <james.holloway94@gmail.com>
2026-06-20 23:51:12 +0000

    settle ssh known algorithm sync time after update

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-20 22:21:00 UTC
+# Last sync: 2026-06-20 23:51:12 UTC
