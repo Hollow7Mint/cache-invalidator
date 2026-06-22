c35f79cc12f5361cc8da9d6661a3326f3ee9074d
James Holloway <james.holloway94@gmail.com>
2026-06-22 17:37:04 +0000

    finalize bastion config sync after nightly rotation

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-22 16:44:03 UTC
+# Last sync: 2026-06-22 17:37:04 UTC
