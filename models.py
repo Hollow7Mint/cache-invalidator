e05ff812140f3eb116cf8df10d27b815c0589a23
James Holloway <james.holloway94@gmail.com>
2026-06-22 02:36:40 +0000

    mirror guardduty archive time in metadata record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-22 01:45:59 UTC
+# Last sync: 2026-06-22 02:36:40 UTC
