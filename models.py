6201377dc14724090f65710c64455e38759c55d7
James Holloway <james.holloway94@gmail.com>
2026-06-23 08:17:44 +0000

    carry waf ip set sync stamp to config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-23 07:33:15 UTC
+# Last sync: 2026-06-23 08:17:43 UTC
