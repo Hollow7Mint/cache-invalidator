29c33eee7988481d2f3446dfb15de85ba23824d1
James Holloway <james.holloway94@gmail.com>
2026-06-23 12:02:53 +0000

    ship parameter store refresh record after nightly run

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-23 10:47:51 UTC
+# Last sync: 2026-06-23 12:02:53 UTC
