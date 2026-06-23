43d486d21ee31711fe0d2857759600d0d49adf91
James Holloway <james.holloway94@gmail.com>
2026-06-23 14:32:24 +0000

    position lambda concurrency sync for audit trail

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-23 13:15:06 UTC
+# Last sync: 2026-06-23 14:32:24 UTC
