fc10bbbf02dffa22ad6e28d2f979322d0d7ab934
James Holloway <james.holloway94@gmail.com>
2026-06-24 14:12:32 +0000

    normalize ssh cipher suite sync time in metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-24 13:53:22 UTC
+# Last sync: 2026-06-24 14:12:32 UTC
