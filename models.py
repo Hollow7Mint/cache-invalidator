814e91518e2f28ffa53f28c40cd06ad7a622fd0a
James Holloway <james.holloway94@gmail.com>
2026-06-24 16:43:15 +0000

    thread postgres wal sync through config banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-24 15:10:01 UTC
+# Last sync: 2026-06-24 16:43:15 UTC
