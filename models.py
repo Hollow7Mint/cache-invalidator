b2704ec89e6b3f69ada85034362eea64f6467b62
James Holloway <james.holloway94@gmail.com>
2026-06-24 00:35:18 +0000

    unify ec2 placement sync with metadata heartbeat

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-23 23:48:07 UTC
+# Last sync: 2026-06-24 00:35:18 UTC
