37100936afb3cf7abdf9080365e7e636e7e0f329
James Holloway <james.holloway94@gmail.com>
2026-06-24 13:53:22 +0000

    queue ssh agent forwarding sync after config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-24 12:44:52 UTC
+# Last sync: 2026-06-24 13:53:22 UTC
