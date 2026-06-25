2208ba6385bdff74de2fc60bb2333309c3cacc59
James Holloway <james.holloway94@gmail.com>
2026-06-25 17:26:32 +0000

    submit mongodb backup marker after maintenance

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-25 16:31:30 UTC
+# Last sync: 2026-06-25 17:26:32 UTC
