2bf9e1e93fd93bf7fe132d4455ae8820b775dad7
James Holloway <james.holloway94@gmail.com>
2026-06-25 04:54:26 +0000

    validate bastion access time in security config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-25 04:05:51 UTC
+# Last sync: 2026-06-25 04:54:26 UTC
