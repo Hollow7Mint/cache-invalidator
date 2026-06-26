4f54b24c945d8b4a2618abde96354c689dc48cf6
James Holloway <james.holloway94@gmail.com>
2026-06-26 20:59:42 +0000

    document postgres index maintenance sync in record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-26 19:54:37 UTC
+# Last sync: 2026-06-26 20:59:42 UTC
