6dc533c86b0ad18afc3e4c2bce11e71ddc277d79
James Holloway <james.holloway94@gmail.com>
2026-06-27 13:34:05 +0000

    trace mysql table statistics sync for fleet audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-27 12:37:22 UTC
+# Last sync: 2026-06-27 13:34:05 UTC
