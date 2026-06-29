ca7f709259807987a1dd8d42457bcc45f255dee9
James Holloway <james.holloway94@gmail.com>
2026-06-29 06:39:14 +0000

    sync ssm parameter refresh after pipeline pass

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-29 05:34:26 UTC
+# Last sync: 2026-06-29 06:39:14 UTC
