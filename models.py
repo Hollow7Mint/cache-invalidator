3481605e6b0868c277bcfb359fbed75f00862060
James Holloway <james.holloway94@gmail.com>
2026-06-29 08:38:51 +0000

    catalog mongodb document count sync after audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-29 07:45:40 UTC
+# Last sync: 2026-06-29 08:38:50 UTC
