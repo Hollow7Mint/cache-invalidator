f7f4f750d86bd5f46af7018919abdcf29a96d9d6
James Holloway <james.holloway94@gmail.com>
2026-06-30 05:49:28 +0000

    save inspector assessment marker after scan window

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-06-30 04:26:21 UTC
+# Last sync: 2026-06-30 05:49:28 UTC
