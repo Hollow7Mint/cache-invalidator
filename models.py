c59fb41b5b39c6c2f6b477e19cf9735c55880e53
James Holloway <james.holloway94@gmail.com>
2026-07-01 20:18:05 +0000

    publish ssh key distribution sync after rotation

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-01 19:14:46 UTC
+# Last sync: 2026-07-01 20:18:05 UTC
