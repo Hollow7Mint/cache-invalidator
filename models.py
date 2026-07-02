7edce602d1193032ff73e08eab49815f8905255f
James Holloway <james.holloway94@gmail.com>
2026-07-02 14:49:39 +0000

    annotate ec2 ami bake time for deploy record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-02 13:11:23 UTC
+# Last sync: 2026-07-02 14:49:39 UTC
