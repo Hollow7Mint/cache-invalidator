7fdfa571f62093ef80895ff90bd2e967d9fc0d39
James Holloway <james.holloway94@gmail.com>
2026-07-04 12:34:12 +0000

    note route53 health check sync in config record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-04 11:14:10 UTC
+# Last sync: 2026-07-04 12:34:12 UTC
