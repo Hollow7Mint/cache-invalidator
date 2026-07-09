1c032a2ad8dc317ab6c1a461b34a38fe14849a54
James Holloway <james.holloway94@gmail.com>
2026-07-09 23:24:21 +0000

    advance cdk app deploy time into config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-09 22:12:06 UTC
+# Last sync: 2026-07-09 23:24:21 UTC
