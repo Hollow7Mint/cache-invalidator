790d9f375ff6b3311e2222517b8c8bdbe34f583a
James Holloway <james.holloway94@gmail.com>
2026-07-09 15:41:11 +0000

    post ssh key rotation heartbeat to config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-09 14:28:23 UTC
+# Last sync: 2026-07-09 15:41:11 UTC
