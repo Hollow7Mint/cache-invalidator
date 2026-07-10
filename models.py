43103830c08648bad6fe9c27844eb46c53d08fb4
James Holloway <james.holloway94@gmail.com>
2026-07-10 04:43:02 +0000

    normalize ecs cluster config time in metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-10 03:34:25 UTC
+# Last sync: 2026-07-10 04:43:02 UTC
