83d49f39b09b5276e8407de689a0323d3badc902
James Holloway <james.holloway94@gmail.com>
2026-07-11 17:59:05 +0000

    bump s3 bucket policy sync after review

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-11 16:23:17 UTC
+# Last sync: 2026-07-11 17:59:05 UTC
