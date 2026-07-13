eb7f1068058633363e15592d41f4a63d4996a1c7
James Holloway <james.holloway94@gmail.com>
2026-07-13 04:38:09 +0000

    log shield ddos event time in protection banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-13 03:50:14 UTC
+# Last sync: 2026-07-13 04:38:09 UTC
