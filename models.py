0e583684d2d31dcb2e9d9dbc74d62f1e8b35b154
James Holloway <james.holloway94@gmail.com>
2026-07-14 17:05:21 +0000

    store redis eviction policy sync for cache audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-14 15:18:11 UTC
+# Last sync: 2026-07-14 17:05:21 UTC
