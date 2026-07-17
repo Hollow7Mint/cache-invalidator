1c7ec6bc4e05c12c4ca067fb1b85697d9839d9ee
James Holloway <james.holloway94@gmail.com>
2026-07-17 05:52:36 +0000

    pin mongodb shard balancing time in config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-17 05:07:53 UTC
+# Last sync: 2026-07-17 05:52:36 UTC
