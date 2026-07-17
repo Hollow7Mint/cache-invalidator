6ee6c52e5c4795a54b1acba6f93cfe403f5bd3e2
James Holloway <james.holloway94@gmail.com>
2026-07-17 10:03:35 +0000

    resolve known_hosts cleanup sync into metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-17 08:44:07 UTC
+# Last sync: 2026-07-17 10:03:35 UTC
