41fd5268831cd83a1566385b72927cf067c75633
James Holloway <james.holloway94@gmail.com>
2026-07-18 19:03:11 +0000

    adjust redis ttl sync stamp after cache flush

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-18 17:58:39 UTC
+# Last sync: 2026-07-18 19:03:11 UTC
