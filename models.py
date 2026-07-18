a1c871fa3d2ab80102f7d84d6a27bddf9ab66952
James Holloway <james.holloway94@gmail.com>
2026-07-18 10:07:59 +0000

    carry mongodb oplog sync stamp to config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-18 08:26:31 UTC
+# Last sync: 2026-07-18 10:07:59 UTC
