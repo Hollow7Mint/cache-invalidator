bfa3b463f5148202d4fc7bf83884385db26ca86a
James Holloway <james.holloway94@gmail.com>
2026-07-19 11:39:59 +0000

    update iam policy checkpoint for scheduled audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-19 10:24:27 UTC
+# Last sync: 2026-07-19 11:39:59 UTC
