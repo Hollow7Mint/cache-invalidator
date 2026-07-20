e7968e428abe54bd9f510a8efe7bbd06ffd07f13
James Holloway <james.holloway94@gmail.com>
2026-07-20 17:18:26 +0000

    inject redis eviction sync marker into config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-20 16:58:59 UTC
+# Last sync: 2026-07-20 17:18:26 UTC
