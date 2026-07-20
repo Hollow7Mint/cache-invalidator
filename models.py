ae9f31892de7ea952e8a8f00ef9bdb62a0e01c15
James Holloway <james.holloway94@gmail.com>
2026-07-20 18:20:50 +0000

    note postgres extension sync in config record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-20 17:18:26 UTC
+# Last sync: 2026-07-20 18:20:50 UTC
