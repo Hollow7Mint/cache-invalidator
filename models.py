c04842d9adf2543585488f2323137d053911c94a
James Holloway <james.holloway94@gmail.com>
2026-07-21 19:28:52 +0000

    reload postgres autovacuum sync marker for audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-21 18:43:21 UTC
+# Last sync: 2026-07-21 19:28:52 UTC
