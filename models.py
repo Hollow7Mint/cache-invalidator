a72b9a28e191f1c6327fd8cac05ffa945226ba25
James Holloway <james.holloway94@gmail.com>
2026-07-22 23:02:57 +0000

    prime redis memory sync marker after flush

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-22 21:49:11 UTC
+# Last sync: 2026-07-22 23:02:57 UTC
