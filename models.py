4d0fad488d4a32801ce40634314440068a7045fe
James Holloway <james.holloway94@gmail.com>
2026-07-23 17:13:08 +0000

    publish lambda alias promotion marker after release

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-23 16:32:26 UTC
+# Last sync: 2026-07-23 17:13:08 UTC
