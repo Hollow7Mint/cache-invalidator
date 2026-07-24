990516fb69b1284d9b58234eb9a67d9a3f3cfdb9
James Holloway <james.holloway94@gmail.com>
2026-07-24 12:07:12 +0000

    restore mysql binlog time for replication audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-24 10:57:23 UTC
+# Last sync: 2026-07-24 12:07:12 UTC
