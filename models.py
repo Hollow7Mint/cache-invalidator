3b628e4b80e55e376a72953826e7ce69b3e5970a
James Holloway <james.holloway94@gmail.com>
2026-07-24 02:37:49 +0000

    keep ecs task sync marker fresh after rollout

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-24 01:42:36 UTC
+# Last sync: 2026-07-24 02:37:49 UTC
