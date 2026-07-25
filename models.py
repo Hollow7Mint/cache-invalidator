84c1db0a81930ce5dcf04079652856bac1901080
James Holloway <james.holloway94@gmail.com>
2026-07-25 17:46:28 +0000

    anchor ssh client config sync after key rotation

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-25 16:29:07 UTC
+# Last sync: 2026-07-25 17:46:28 UTC
