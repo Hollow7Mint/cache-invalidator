7386e26e1d4469ec49803298b3b8d3fbd17c1fc4
James Holloway <james.holloway94@gmail.com>
2026-07-25 04:06:26 +0000

    retain redis aof sync stamp for persistence audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-25 02:32:11 UTC
+# Last sync: 2026-07-25 04:06:26 UTC
