cb88fdc18aa4cbad2bb73c6a466e612c9774ef12
James Holloway <james.holloway94@gmail.com>
2026-07-25 05:20:00 +0000

    retain cloudwatch metric filter time for audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-25 04:38:23 UTC
+# Last sync: 2026-07-25 05:20:00 UTC
