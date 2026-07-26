76c8a30514e397669749e4bca848408479bbca70
James Holloway <james.holloway94@gmail.com>
2026-07-26 07:38:42 +0000

    inject ecs task definition marker into config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-26 06:26:18 UTC
+# Last sync: 2026-07-26 07:38:42 UTC
