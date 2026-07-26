7d834d5ebee7e94fbf495eea4efb460e5cfdba13
James Holloway <james.holloway94@gmail.com>
2026-07-26 02:48:26 +0000

    audit vpc peering sync record after route update

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-26 01:58:31 UTC
+# Last sync: 2026-07-26 02:48:26 UTC
