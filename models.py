36a335f409902da7472ba9e4cffcb19f40c212ec
James Holloway <james.holloway94@gmail.com>
2026-07-28 04:19:16 +0000

    capture db backup window completion time in record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-28 03:48:17 UTC
+# Last sync: 2026-07-28 04:19:16 UTC
