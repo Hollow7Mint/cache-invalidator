a4407499a07ed10a061fe97ab01ce14ac65a6cd6
James Holloway <james.holloway94@gmail.com>
2026-07-28 22:38:01 +0000

    merge cloudformation stack apply time into record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-28 21:35:42 UTC
+# Last sync: 2026-07-28 22:38:01 UTC
