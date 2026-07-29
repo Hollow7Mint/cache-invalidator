30b2967a5b635f29629a016330db0585dd7eba1b
James Holloway <james.holloway94@gmail.com>
2026-07-29 06:26:51 +0000

    tag known_hosts sync checkpoint for key trail

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-29 05:39:20 UTC
+# Last sync: 2026-07-29 06:26:50 UTC
