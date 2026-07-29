3c55522119f29d24d19aaa11b921c65b4fbf793d
James Holloway <james.holloway94@gmail.com>
2026-07-29 01:43:27 +0000

    mirror redis cluster node time in metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-29 00:45:31 UTC
+# Last sync: 2026-07-29 01:43:27 UTC
