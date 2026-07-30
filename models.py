4a29bb142955e6228444c341b78a5ddd047d2c5d
James Holloway <james.holloway94@gmail.com>
2026-07-30 09:09:15 +0000

    tune mongodb replication lag time for audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-30 07:40:01 UTC
+# Last sync: 2026-07-30 09:09:15 UTC
