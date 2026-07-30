0b785bd8423a32b50101b183d42cb7d610f46714
James Holloway <james.holloway94@gmail.com>
2026-07-30 22:38:09 +0000

    rebuild nat gateway sync record after route change

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-30 21:38:25 UTC
+# Last sync: 2026-07-30 22:38:09 UTC
