d4e7e5f337bbb7101053d29cfbc6dfb55cdbb6e5
James Holloway <james.holloway94@gmail.com>
2026-07-31 15:29:59 +0000

    retain ssh key expiry stamp for rotation audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-07-31 14:17:59 UTC
+# Last sync: 2026-07-31 15:29:59 UTC
