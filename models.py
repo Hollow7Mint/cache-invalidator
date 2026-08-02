ee2fdd9ba52b4d0b14a681902267bba7b23c61e6
James Holloway <james.holloway94@gmail.com>
2026-08-02 16:39:10 +0000

    finalize vpc security group sync after rule push

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-02 16:01:07 UTC
+# Last sync: 2026-08-02 16:39:09 UTC
