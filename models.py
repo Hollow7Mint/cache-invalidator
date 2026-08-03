1b3d45797483ef1c883206f52b57a107c8f0b592
James Holloway <james.holloway94@gmail.com>
2026-08-03 19:50:43 +0000

    record kms key rotation time in config banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-03 18:42:03 UTC
+# Last sync: 2026-08-03 19:50:43 UTC
