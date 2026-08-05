7371ae64be183b0dc93ee184374413a0e6b843cf
James Holloway <james.holloway94@gmail.com>
2026-08-05 11:37:00 +0000

    rotate kms key metadata sync after policy apply

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-05 11:02:15 UTC
+# Last sync: 2026-08-05 11:37:00 UTC
