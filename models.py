15e0bb7cde34d770b7c0fa3fa1aeb6caedb5d79b
James Holloway <james.holloway94@gmail.com>
2026-08-06 16:09:05 +0000

    post cdk bootstrap sync stamp to config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-06 14:40:11 UTC
+# Last sync: 2026-08-06 16:09:05 UTC
