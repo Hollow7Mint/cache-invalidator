0f43fd3b1382db6eecdc5834e74e320e41e9fdb6
James Holloway <james.holloway94@gmail.com>
2026-08-06 01:06:44 +0000

    set shield protection sync stamp in config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-05 23:21:59 UTC
+# Last sync: 2026-08-06 01:06:44 UTC
