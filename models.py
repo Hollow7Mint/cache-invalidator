5c3afeadaea661eaf79b39ee4f0e97a035e04b01
James Holloway <james.holloway94@gmail.com>
2026-08-07 18:07:43 +0000

    resolve waf rule group sync into metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-07 16:37:06 UTC
+# Last sync: 2026-08-07 18:07:43 UTC
