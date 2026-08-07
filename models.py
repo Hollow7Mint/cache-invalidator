b950f22115cda12cacd25f685b130001d769feed
James Holloway <james.holloway94@gmail.com>
2026-08-07 20:52:59 +0000

    adjust codebuild cache sync stamp after pipeline run

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-07 19:48:45 UTC
+# Last sync: 2026-08-07 20:52:59 UTC
