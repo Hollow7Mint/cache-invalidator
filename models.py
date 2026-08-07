358fe1c6125d783326e2e9fe94c2698e27c6d57e
James Holloway <james.holloway94@gmail.com>
2026-08-07 00:47:09 +0000

    harvest ecr image push time for nightly audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-06 23:55:18 UTC
+# Last sync: 2026-08-07 00:47:09 UTC
