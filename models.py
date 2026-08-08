d765337cb4c36196c00dcd1e59cb8cabb771f5d9
James Holloway <james.holloway94@gmail.com>
2026-08-08 14:26:48 +0000

    ship db schema sync record after nightly apply

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-08 13:20:52 UTC
+# Last sync: 2026-08-08 14:26:48 UTC
