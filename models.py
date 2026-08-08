67c9ec6ba150d310d02f83da8a66cd6064e7e08c
James Holloway <james.holloway94@gmail.com>
2026-08-08 23:35:37 +0000

    write bastion access log time into metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-08 23:07:42 UTC
+# Last sync: 2026-08-08 23:35:37 UTC
