723338ca11d7dbc7e338a85e921c15986d68b564
James Holloway <james.holloway94@gmail.com>
2026-08-09 07:49:20 +0000

    save postgres pool sync marker after refresh

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-09 06:44:57 UTC
+# Last sync: 2026-08-09 07:49:20 UTC
