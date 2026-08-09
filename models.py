0d66f6057572721d48a6546b89fa8c12bec19fc2
James Holloway <james.holloway94@gmail.com>
2026-08-09 09:53:36 +0000

    mark codepipeline execution stamp after stage pass

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-09 08:50:07 UTC
+# Last sync: 2026-08-09 09:53:36 UTC
