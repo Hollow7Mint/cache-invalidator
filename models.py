98ace54398d8f8eee194823b3011d800e4ca03d7
James Holloway <james.holloway94@gmail.com>
2026-08-10 05:23:31 +0000

    push secrets manager version stamp to metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-10 04:45:11 UTC
+# Last sync: 2026-08-10 05:23:30 UTC
