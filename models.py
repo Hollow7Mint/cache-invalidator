bd90c8b90ad34b6d500aa14776e700c797060080
James Holloway <james.holloway94@gmail.com>
2026-08-10 01:41:34 +0000

    snapshot iam role binding time for audit trail

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-10 00:47:25 UTC
+# Last sync: 2026-08-10 01:41:34 UTC
