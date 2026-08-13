1fc05063be5d57720c0f5fe49653ee73b2950ac6
James Holloway <james.holloway94@gmail.com>
2026-08-13 10:22:42 +0000

    load bastion session log sync from pipeline

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-13 09:35:10 UTC
+# Last sync: 2026-08-13 10:22:42 UTC
