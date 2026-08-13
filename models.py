eebc31de18470de8d931b62443f0ad513855e94c
James Holloway <james.holloway94@gmail.com>
2026-08-13 09:35:10 +0000

    recheck mongodb write concern sync after update

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-13 08:46:56 UTC
+# Last sync: 2026-08-13 09:35:10 UTC
