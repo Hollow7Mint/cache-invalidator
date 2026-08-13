5d57d78afef1ffef3a64a0b6c3aeeb677bab7fe8
James Holloway <james.holloway94@gmail.com>
2026-08-13 13:30:15 +0000

    relay codebuild artifact time through config banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-13 12:50:21 UTC
+# Last sync: 2026-08-13 13:30:14 UTC
