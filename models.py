ed355973d5a9b48e74cf0a76ca2890cfabf7545a
James Holloway <james.holloway94@gmail.com>
2026-08-15 01:00:23 +0000

    tune bastion audit log sync time for review

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-14 23:27:50 UTC
+# Last sync: 2026-08-15 01:00:23 UTC
