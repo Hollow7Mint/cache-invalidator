07e7be36d81def280b3cb89616611245124babfd
James Holloway <james.holloway94@gmail.com>
2026-08-16 21:36:51 +0000

    push ssh config sync marker after host refresh

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-16 20:42:09 UTC
+# Last sync: 2026-08-16 21:36:51 UTC
