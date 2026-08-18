117d33864e49c322fd8181bc41ae402d5d281a8d
James Holloway <james.holloway94@gmail.com>
2026-08-18 19:35:07 +0000

    restamp iam permission boundary sync after review

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-18 18:24:09 UTC
+# Last sync: 2026-08-18 19:35:07 UTC
