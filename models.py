1cdc73028b867c638f41834291e8ce1f44216f76
James Holloway <james.holloway94@gmail.com>
2026-08-18 03:52:01 +0000

    normalize mysql charset sync time in metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-18 02:57:27 UTC
+# Last sync: 2026-08-18 03:52:01 UTC
