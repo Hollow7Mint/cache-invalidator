95ec95d7d870216db1dfb57266f91c41db42eb79
James Holloway <james.holloway94@gmail.com>
2026-08-19 21:36:15 +0000

    ship ssh key sync record after nightly apply

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-19 21:02:00 UTC
+# Last sync: 2026-08-19 21:36:15 UTC
