9ac941a4db756fa06ad5f9c8ef6a78f53878ff99
James Holloway <james.holloway94@gmail.com>
2026-08-19 22:42:59 +0000

    transfer kms alias time to config banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-19 21:36:15 UTC
+# Last sync: 2026-08-19 22:42:59 UTC
