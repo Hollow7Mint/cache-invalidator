018aa9311499d597295f7deec6f2cea49a5b2928
James Holloway <james.holloway94@gmail.com>
2026-08-19 17:45:20 +0000

    preserve nat gateway sync time in infra banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-19 16:12:34 UTC
+# Last sync: 2026-08-19 17:45:20 UTC
