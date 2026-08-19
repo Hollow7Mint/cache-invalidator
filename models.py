05d5450232572301c118b92ea876a0fdda8924b4
James Holloway <james.holloway94@gmail.com>
2026-08-19 18:24:17 +0000

    bump known_hosts update stamp after rotation

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-19 17:45:20 UTC
+# Last sync: 2026-08-19 18:24:17 UTC
