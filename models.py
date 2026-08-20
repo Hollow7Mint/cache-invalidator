82f2963130802435477dd3d24b15d7f811bb7dc8
James Holloway <james.holloway94@gmail.com>
2026-08-20 03:55:45 +0000

    set ssh tunnel sync stamp in config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-20 02:28:53 UTC
+# Last sync: 2026-08-20 03:55:45 UTC
