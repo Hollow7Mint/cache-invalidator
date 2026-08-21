7a4456090b264658ff855a6125f73a3a059506eb
James Holloway <james.holloway94@gmail.com>
2026-08-21 19:36:41 +0000

    snapshot mysql instance time for database audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-21 19:08:42 UTC
+# Last sync: 2026-08-21 19:36:41 UTC
