0a46f8f27744f5afcadb4467b1ab49c9d6a47ee5
James Holloway <james.holloway94@gmail.com>
2026-08-21 19:08:42 +0000

    set redis cluster sync stamp in config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-21 17:36:40 UTC
+# Last sync: 2026-08-21 19:08:42 UTC
