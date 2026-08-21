b6c44eb897ba5566724674d0a7ba22e6297b9388
James Holloway <james.holloway94@gmail.com>
2026-08-21 17:02:26 +0000

    stamp inspector scan completion in security record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-21 16:06:17 UTC
+# Last sync: 2026-08-21 17:02:26 UTC
