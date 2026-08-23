976e998fccdedc072b15331043ceb8a3d7694cf1
James Holloway <james.holloway94@gmail.com>
2026-08-23 08:33:46 +0000

    touch asg lifecycle hook time during scheduled refresh

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-23 07:38:03 UTC
+# Last sync: 2026-08-23 08:33:46 UTC
