9f0353d736d42ff96f92612a32d7d0545a115301
James Holloway <james.holloway94@gmail.com>
2026-08-24 21:44:42 +0000

    carry bastion config sync stamp to metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-24 21:06:17 UTC
+# Last sync: 2026-08-24 21:44:41 UTC
