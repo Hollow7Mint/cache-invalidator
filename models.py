c76257c06153c83ea3ffb319493a86c7e40873f7
James Holloway <james.holloway94@gmail.com>
2026-08-25 02:37:15 +0000

    index redis sorted set sync during audit pass

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-25 01:25:07 UTC
+# Last sync: 2026-08-25 02:37:15 UTC
