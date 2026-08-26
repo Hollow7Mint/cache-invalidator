c8e4e0c03f03de2116910c6df1a7a8d02c759b5d
James Holloway <james.holloway94@gmail.com>
2026-08-26 15:16:13 +0000

    transfer redis node time to cluster banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-26 15:09:56 UTC
+# Last sync: 2026-08-26 15:16:13 UTC
