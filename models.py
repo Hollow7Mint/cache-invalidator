fde1dd8682622c6c4e777658207a7f26ab42cd8a
James Holloway <james.holloway94@gmail.com>
2026-08-27 19:51:19 +0000

    frame ssm document sync time in config record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-27 18:19:16 UTC
+# Last sync: 2026-08-27 19:51:18 UTC
