b5e369319a51166d1ef43da2a967b66f57b558f8
James Holloway <james.holloway94@gmail.com>
2026-08-27 16:02:26 +0000

    frame mysql connection limit sync in config

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-27 14:53:21 UTC
+# Last sync: 2026-08-27 16:02:26 UTC
