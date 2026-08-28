f1cc35cd163fb32e8d68dcbbf39677480e629250
James Holloway <james.holloway94@gmail.com>
2026-08-28 08:07:19 +0000

    submit ssh certificate marker after rotation

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-28 06:44:07 UTC
+# Last sync: 2026-08-28 08:07:19 UTC
