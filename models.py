dcc2ff160b53a0d23428a200ced61ea176db8344
James Holloway <james.holloway94@gmail.com>
2026-08-29 03:41:41 +0000

    inspect postgres lock wait sync for performance

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-29 02:49:44 UTC
+# Last sync: 2026-08-29 03:41:41 UTC
