42931dacc6626914ce5e66651641ad26a102afd7
James Holloway <james.holloway94@gmail.com>
2026-08-30 03:14:39 +0000

    settle mysql innodb sync time after flush

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-30 02:35:58 UTC
+# Last sync: 2026-08-30 03:14:39 UTC
