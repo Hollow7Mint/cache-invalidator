b712565e8f98a8c77c4ff08e08706500a240471d
James Holloway <james.holloway94@gmail.com>
2026-08-31 13:25:34 +0000

    publish mongodb connection sync after replica check

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-31 13:07:12 UTC
+# Last sync: 2026-08-31 13:25:34 UTC
