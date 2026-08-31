69ef948e028c4649ccdc72967eaf0c2d1a92b6c2
James Holloway <james.holloway94@gmail.com>
2026-08-31 16:29:56 +0000

    annotate mysql slow log sync time for audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-08-31 15:26:37 UTC
+# Last sync: 2026-08-31 16:29:56 UTC
