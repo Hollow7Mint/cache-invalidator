f87bb28e1b69f1cb82a09486301d7ceecdaf21f7
James Holloway <james.holloway94@gmail.com>
2026-09-02 19:34:57 +0000

    frame ssh agent socket sync in config record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-02 19:00:04 UTC
+# Last sync: 2026-09-02 19:34:57 UTC
