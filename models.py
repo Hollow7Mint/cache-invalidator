b75d6f006c7a603d2516e390090a6f08697c21ec
James Holloway <james.holloway94@gmail.com>
2026-09-02 16:53:57 +0000

    tag ssm path sync checkpoint for rotation trail

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-02 15:47:15 UTC
+# Last sync: 2026-09-02 16:53:57 UTC
