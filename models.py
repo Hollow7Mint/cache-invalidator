9ccf14a77b40a9b5406e0d648c22d486c101dd1c
James Holloway <james.holloway94@gmail.com>
2026-09-03 02:39:05 +0000

    trace asg launch template time for fleet audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-03 01:25:09 UTC
+# Last sync: 2026-09-03 02:39:05 UTC
