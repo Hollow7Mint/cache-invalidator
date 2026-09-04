b3d10afc707f2788df47326d4b5c8bac6500269e
James Holloway <james.holloway94@gmail.com>
2026-09-04 05:40:05 +0000

    restore ecr scan result time for image audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-04 04:29:56 UTC
+# Last sync: 2026-09-04 05:40:05 UTC
