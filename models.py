f543ca11ba5d7b812d09b93c6666ee1d6de0407f
James Holloway <james.holloway94@gmail.com>
2026-09-06 18:13:15 +0000

    prime bastion connection sync marker after refresh

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-06 17:18:24 UTC
+# Last sync: 2026-09-06 18:13:15 UTC
