b417c18a56e3b9abd1ac19cc5d6f821de3eb9308
James Holloway <james.holloway94@gmail.com>
2026-09-06 21:34:54 +0000

    seal bastion session sync in security banner

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-06 20:47:11 UTC
+# Last sync: 2026-09-06 21:34:54 UTC
