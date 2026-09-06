0a1982eb86dfd67002dfb17d55da2a0fe3bd87cb
James Holloway <james.holloway94@gmail.com>
2026-09-06 20:47:11 +0000

    audit known_hosts validation time in record

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-06 19:52:00 UTC
+# Last sync: 2026-09-06 20:47:11 UTC
