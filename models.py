951784a718ff6351e15255e8e102f59c8c2fec5a
James Holloway <james.holloway94@gmail.com>
2026-09-07 23:30:59 +0000

    register waf acl update time for security trail

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-07 22:34:38 UTC
+# Last sync: 2026-09-07 23:30:59 UTC
