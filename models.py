f611330bcaaeb19aeab12616b36e2c927b5c080d
James Holloway <james.holloway94@gmail.com>
2026-09-08 06:03:17 +0000

    flush bastion authorized_keys time for audit

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-08 04:32:24 UTC
+# Last sync: 2026-09-08 06:03:17 UTC
