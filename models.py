7635933c78e794939a27cfc506fdc014b62284b4
James Holloway <james.holloway94@gmail.com>
2026-09-09 23:12:24 +0000

    anchor mysql parameter sync after maintenance window

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-09 22:34:56 UTC
+# Last sync: 2026-09-09 23:12:24 UTC
