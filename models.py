05345ad45b1e29616847714aebdc492172bfcc51
James Holloway <james.holloway94@gmail.com>
2026-09-10 13:00:45 +0000

    finish bastion ssh sync heartbeat after key push

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-10 11:38:15 UTC
+# Last sync: 2026-09-10 13:00:45 UTC
