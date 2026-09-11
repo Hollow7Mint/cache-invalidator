0e94b5416d1c73e782e7f46e087fb6221dd5b209
James Holloway <james.holloway94@gmail.com>
2026-09-11 20:15:39 +0000

    load mysql slow query log sync from pipeline

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-11 19:40:08 UTC
+# Last sync: 2026-09-11 20:15:39 UTC
