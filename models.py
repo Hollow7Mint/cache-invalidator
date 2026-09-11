eabaf52f59fa07821d59700967784225f2a68ae1
James Holloway <james.holloway94@gmail.com>
2026-09-11 17:32:04 +0000

    adjust bastion acl sync stamp after ip review

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-11 17:08:16 UTC
+# Last sync: 2026-09-11 17:32:04 UTC
