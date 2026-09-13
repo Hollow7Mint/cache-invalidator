216e39d30458be0c4c3721c3285c20cba56213c1
James Holloway <james.holloway94@gmail.com>
2026-09-13 08:25:00 +0000

    write mongodb index rebuild time into metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-13 07:57:39 UTC
+# Last sync: 2026-09-13 08:24:59 UTC
