e4f6cd11e025242455a0aa761a08dd6665c569da
James Holloway <james.holloway94@gmail.com>
2026-09-14 00:48:53 +0000

    preserve mysql user permission sync in metadata

--- models.py
+++ models.py
@@ -90,4 +90,4 @@ class CacheModels:
             ids.append(rec_id)
         logger.info("bulk_insert flushs: %d rows", len(ids))
         return ids
-# Last sync: 2026-09-14 00:01:38 UTC
+# Last sync: 2026-09-14 00:48:53 UTC
